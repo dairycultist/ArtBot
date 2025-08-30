a49cd66b530f6ecfa295725b1264bd8d7470d1f5
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-30 01:36:46 -0400

    Fix reading from file, refactor

--- main.js
+++ main.js
@@ -5,84 +5,91 @@ const ask = require("readline-sync"); // npm install readline-sync
 
     while (true) {
 
-        const command = ask.question("\x1b[32mArtBot$ \x1b[0m").trim();
+        let command = ask.question("\x1b[32mArtBot$ \x1b[0m").trim();
+
         const commandName = command.substring(0, command.includes(" ") ? command.indexOf(" ") : command.length);
 
-        if (commandName == "help") {
+        try {
 
-            // log helpful message on commands
-            console.log(`
-draw
-    -gradio ID              \x1b[2mthe gradio id of your gradio link (i.e. https://<THIS_PART>.gradio.live/)\x1b[0m
-    -pos POSITIVE_PROMPT
-    [-neg NEGATIVE_PROMPT]
-    [-size WIDTHxHEIGHT]    \x1b[2mdefault is 1200x1200. minimum size is 640x640\x1b[0m
-    [-count COUNT]          \x1b[2mdefault is 1\x1b[0m
-    [-seed SEED]            \x1b[2mdefault is -1 (random)\x1b[0m
-    [-steps STEPS]          \x1b[2m(not implemented yet) default is 50\x1b[0m
-    [-in FILE]              \x1b[2mif specified, reads additional arguments from a file and behaves as if appending the content of the file to the end of the command (i.e. the file follows the same argument structure)\x1b[0m
-    [-out DIRECTORY]        \x1b[2m(not implemented yet) output folder to put generated images into. default is ./ (current directory)\x1b[0m
-    [-bg]                   \x1b[2m(not implemented yet) run generator in the background, doesn't announce when it finishes, and you can continue to queue more\x1b[0m
-            `);
+            if (commandName == "help") {
 
-            // loras (lists loras)
-            // model [list|use MODEL]
-            // stash DIRECTORY | uploads all images in the DIRECTORY to stash as separate posts with tags automatically added based on the prompt (what about description? title?)
-            // analyze IMAGE_PATH | reconstruct a draw command based on the image's metadata
+                // log helpful message on commands
+                console.log(`
+    draw
+        -gradio ID              \x1b[2mthe gradio id of your gradio link (i.e. https://<THIS_PART>.gradio.live/)\x1b[0m
+        -pos POSITIVE_PROMPT
+        [-neg NEGATIVE_PROMPT]
+        [-size WIDTHxHEIGHT]    \x1b[2mdefault is 1200x1200. minimum size is 640x640\x1b[0m
+        [-count COUNT]          \x1b[2mdefault is 1\x1b[0m
+        [-seed SEED]            \x1b[2mdefault is -1 (random)\x1b[0m
+        [-steps STEPS]          \x1b[2m(not implemented yet) default is 50\x1b[0m
+        [-in FILE]              \x1b[2mif specified, reads additional arguments from a file and behaves as if appending the content of the file to the end of the command (i.e. the file follows the same argument structure)\x1b[0m
+        [-out DIRECTORY]        \x1b[2m(not implemented yet) output folder to put generated images into. default is ./ (current directory)\x1b[0m
+        [-bg]                   \x1b[2m(not implemented yet) run generator in the background, doesn't announce when it finishes, and you can continue to queue more\x1b[0m
+                `);
 
-        } else if (commandName == "draw" && command.includes("-gradio") && command.includes("-pos")) {
+                // loras (lists loras)
+                // model [list|use MODEL]
+                // stash DIRECTORY | uploads all images in the DIRECTORY to stash as separate posts with tags automatically added based on the prompt (what about description? title?)
+                // analyze IMAGE_PATH | reconstruct a draw command based on the image's metadata
 
-            // check for -in (done first because it may add new arguments)
-            const inFile = getCommandArgument(command, "in");
+            } else if (commandName == "draw") {
 
-            if (inFile && fs.existsSync(inFile)) {
+                // check for -in (done first because it may add new arguments)
+                const inFile = getCommandArgument(command, "in");
 
-                command += " " + fs.readFileSync(inFile, "utf8");
-            }
+                if (inFile && fs.existsSync(inFile)) {
 
-            // get arguments
-            const gradio = getCommandArgument(command, "gradio");
-            const pos = getCommandArgument(command, "pos");
-            const neg = getCommandArgument(command, "neg");
-            
-            const [ width, height ] = (() => {
-                let size = getCommandArgument(command, "size");
-
-                if (!size) {
-                    return [ 1200, 1200 ];
+                    command += " " + fs.readFileSync(inFile, "utf8");
                 }
 
-                size = size.split("x");
+                // validate
+                if (!command.includes("-gradio") || !command.includes("-pos")) {
 
-                if (size.length != 2 || Number(size[0]) == NaN || Number(size[1]) == NaN) {
-                    return [ 1200, 1200 ];
+                    throw new Error("Missing -gradio or -pos arguments.");
                 }
 
-                return [ Math.max(640, Number(size[0])),  Math.max(640, Number(size[1])) ];
-            })();
+                // get arguments
+                const gradio = getCommandArgument(command, "gradio");
+                const pos = getCommandArgument(command, "pos");
+                const neg = getCommandArgument(command, "neg");
+                
+                const [ width, height ] = (() => {
+                    let size = getCommandArgument(command, "size");
 
-            const count = (() => {
-                let count = getCommandArgument(command, "count");
-                return count && Number(count) != NaN && Number(count) >= 1 ? Math.floor(Number(count)) : 1;
-            })();
+                    if (!size) {
+                        return [ 1200, 1200 ];
+                    }
 
-            const seed = (() => {
-                let seed = getCommandArgument(command, "seed");
-                return seed ? seed : -1;
-            })();
+                    size = size.split("x");
 
-            console.log(`
+                    if (size.length != 2 || Number(size[0]) == NaN || Number(size[1]) == NaN) {
+                        return [ 1200, 1200 ];
+                    }
+
+                    return [ Math.max(640, Number(size[0])),  Math.max(640, Number(size[1])) ];
+                })();
+
+                const count = (() => {
+                    let count = getCommandArgument(command, "count");
+                    return count && Number(count) != NaN && Number(count) >= 1 ? Math.floor(Number(count)) : 1;
+                })();
+
+                const seed = (() => {
+                    let seed = getCommandArgument(command, "seed");
+                    return seed ? seed : -1;
+                })();
+
+                console.log(`
 \x1b[2mgradio link:\x1b[0m https://${ gradio }.gradio.live/
 \x1b[2mpositive:   \x1b[0m ${ pos }
 \x1b[2mnegative:   \x1b[0m ${ neg }
 \x1b[2msize:       \x1b[0m ${ width }x${ height }
 \x1b[2mcount:      \x1b[0m ${ count }
 \x1b[2mseed:       \x1b[0m ${ seed }
-            `);
+                `);
 
-            // attempt to generate (will fail if API cannot be polled)
-            try {
-
+                // attempt to generate (will fail if API cannot be polled)
                 for (let i = 0; i < count; i++) {
 
                     console.log(`${ i }/${ count } images complete.`);
@@ -101,13 +108,14 @@ draw
 
                 console.log(`${ count }/${ count } images complete.`);
 
-            } catch (err) {
-
-                console.log("Error while generating: " + err);
+            } else {
+                
+                throw new Error("Unrecognized command. Type \"help\" for help.");
             }
 
-        } else {
-            console.log("Unrecognized command/missing arguments. Type \"help\" for help.");
+        } catch (err) {
+
+            console.log("\x1b[31m" + err + "\x1b[0m");
         }
     }
 
