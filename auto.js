d284fbb95fb49d77b395eb57fc004ea2dca5d909
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-10-20 03:44:43 -0400

    Refine composition

--- auto.js
+++ auto.js
@@ -3,6 +3,8 @@ const ask = require("readline-sync");
 const seedrandom = require("seedrandom"); // npm install seedrandom
 const { Jimp } = require("jimp"); // npm install --save jimp
 
+// https://jimp-dev.github.io/jimp/guides/getting-started/
+
 const gradioID = ask.question("Enter gradio ID: ");
 
 // verify gradioID is valid by pinging, if not valid then quit program
@@ -35,68 +37,55 @@ async function generatePost(seed) {
 
 	const getRandom = seedrandom(seed);
 
-	const getRandomOf = (array) => {
-
-		return array[Math.floor(array.length * getRandom())];
-	};
+	const getRandomOf = array => array[Math.floor(array.length * getRandom())];
 
 	const basePos = 
-		`<lora:Immobile_USSBBW_Concept_Lora_for_Illustrious-XL:0.05> <lora:HYPv1-4:0.5> <lora:SyMix_NoobAI_epred_v1_1__fromE7_v01a01:1>
-		<lora:Weather_shine_pupils_mix:1> <lora:KrekkovLycoXLV2:0.5> perfect anatomy, masterpiece, soft lighting, faded colors, (1woman, betterwithsalt), white background, `;
+		"<lora:HYPv1-4:0.5> <lora:SyMix_NoobAI_epred_v1_1__fromE7_v01a01:0.5> (1woman, white background:1.4), " +
 
-	// seeded character/clothing/emotion prompt
-	const headPos =
+		"standing, dynamic pose, front view, full body, " +
+
+		(getRandom() > 0.8 ? "" : "witch hat, ") +
+
 		getRandomOf([ "red hair", "blue hair", "blonde hair", "pink hair", "black hair" ]) + ", " + getRandomOf([ "long hair", "short hair", "ponytail" ]) + ", " +
-		getRandom() > 0.5 ? "tsurime, " : "tareme, " + getRandomOf([ "smug", "smile", "grin", "sad", "pout", "angry" ]) + ", ";
 	
-	const torsoPos =
+		"huge breasts, soft breasts, chubby, wide shoulders, " +
 		getRandomOf([ "red", "blue", "yellow", "pink", "white", "black" ]) + " " +
-		getRandomOf([ "tight t-shirt", "jacket", "hoodie", "loose t-shirt", "crop top", "tube top", "bra", "bikini top" ]) + ", ";
+		getRandomOf([ "tight t-shirt", "jacket", "hoodie", "loose t-shirt", "crop top", "tube top", "bra", "bikini top" ]) + ", " +
 
-	const hipsPos =
-		getRandomOf([ "red", "blue", "yellow", "pink", "white", "black" ]) + " " + getRandomOf([ "jean shorts", "yoga pants", "tights", "miniskirt", "bikini bottom" ]) + ", ";
+		"wide hips, " +
+		getRandomOf([ "red", "blue", "yellow", "pink", "white", "black" ]) + " " + getRandomOf([ "jean shorts", "yoga pants", "tights", "miniskirt", "bikini bottom" ]) + ", "
+		
+		getRandomOf([ "sandals", "sneakers", "barefoot", "heels" ]) + ", ";
 
-	// output images + stitched images matrix
+	// output images (minding the composition!)
 	await generateImage(`output/${ seed }_1.png`, {
-		pos: basePos + "standing, looking at viewer, dynamic pose, front view, full body, wide hips, squishy belly, breasts, soft breasts, soft belly, chubby, chubby face, wide shoulders, exposed belly, " + headPos + torsoPos + hipsPos,
+		pos: "front view, looking at viewer, chubby face, squishy belly, soft belly, exposed belly, " + (getRandom() > 0.5 ? "tsurime, " : "tareme, ") + getRandomOf([ "smug", "smile", "grin", "sad", "pout", "angry" ]) + ", " + basePos,
 		neg: "ugly, blurry, nose, sweat, monochrome",
 		seed: seed,
 		steps: 30,
 		cfg: 6,
-		width: 800,
+		width: 1000,
 		height: 1600
 	});
 
 	await generateImage(`output/${ seed }_2.png`, {
-		pos: basePos + "looking away, expressive, dynamic pose, front view, bust shot, headshot, chubby face, " + headPos + `(${ torsoPos }:0.4)`,
-		neg: "ugly, blurry, nose, sweat, monochrome, legs, body, touching edge of image, offscreen",
-		seed: seed + 10,
+		pos: "view from behind, looking away, fat ass, round ass, " + basePos,
+		neg: "ugly, blurry, nose, sweat, monochrome",
+		seed: seed,
 		steps: 30,
 		cfg: 6,
-		width: 800,
-		height: 800
+		width: 1000,
+		height: 1600
 	});
 
-	await generateImage(`output/${ seed }_3.png`, {
-		pos: basePos + "lower body, (view from behind), fat ass, wide hips, chubby, " + hipsPos + `(${ torsoPos }:0.4)`,
-		neg: "upper body, head, face, front view, ugly, blurry, nose, sweat, monochrome, touching edge of image, offscreen",
-		seed: seed + 10,
-		steps: 30,
-		cfg: 6,
-		width: 800,
-		height: 800
-	});
-
-	// compose matrix
-	const matrix = new Jimp({ width: 1600, height: 1600, color: 0xFFFFFFFF });
+	// stitch together matrix
+	const matrix = new Jimp({ width: 2000, height: 1600, color: 0xFFFFFFFF });
 
 	const image1 = await Jimp.read(`output/${ seed }_1.png`);
 	const image2 = await Jimp.read(`output/${ seed }_2.png`);
-	const image3 = await Jimp.read(`output/${ seed }_3.png`);
 
 	matrix.composite(image1, 0, 0);
-	matrix.composite(image2, 800, 0);
-	matrix.composite(image3, 800, 800);
+	matrix.composite(image2, 1000, 0);
 
 	matrix.write(`output/${ seed }_matrix.png`);
 }
