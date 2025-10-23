7266966ee76029d78d32504be7028eef25599127
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-10-23 01:32:19 -0400

    Simplify and diagnose

--- auto.js
+++ auto.js
@@ -57,58 +57,43 @@ async function generatePost(seed) {
 	const getRandom   = seedrandom(seed);
 	const getRandomOf = array => array[Math.floor(array.length * getRandom())];
 
-	// create prompts for composition
+	/*
+	 * create style prompt
+	 */
 	const colors = [];
 
 	for (let i = 0; i < 2; i++)
 		colors.push(getRandomOf([ "red", "light blue", "dark blue", "light green", "dark green", "yellow", "orange", "pink", "white", "grey", "black", "brown" ]));
 
-	// <lora:SyMix_NoobAI_epred_v1_1__fromE7_v01a01:0.5>
 	let basePos =
-		"<lora:HYPv1-4:1> <lora:Breast_Implants_V2.5:1> <lora:DetailedFur:1> <lora:LaBiuda_IL_Style:0.5> " +
+		"<lora:HYPv1-4:0.5> <lora:Breast_Implants_V2.5:1> <lora:DetailedFur:1> <lora:LaBiuda_IL_Style:0.5> " +
 		"(solo, cowboy shot), (white background:1.5), (anthro, furry_female, fluffy fur:1.4), dynamic pose, standing, " +
-		"lit from behind, light from behind, bright colors, perfect shading, (soft shading), hips, thick thighs, narrow waist, sexy, (gigantic breasts:1.3), tall, adult, " +
-		"L4B1ud4, squinting, (tsurime, eyeliner, black eyeshadow, smug, wide smirk, bedroom eyes, calm:1.2), perfect eyes, very detailed eyes, bangs, large eyes, large mouth, closed mouth, short snout, ";
+		"lit from behind, light from behind, bright colors, perfect shading, (soft shading), hips, thick thighs, narrow waist, sexy, tall, adult, hands on hips, " +
+		"L4B1ud4, squinting, (tsurime, eyeliner, black eyeshadow, smug, wide smirk, bedroom eyes, calm:1.2), perfect eyes, very detailed eyes, bangs, large eyes, short snout, ";
 	
-	let frontPos = "(front view), leaning back, directly in front, looking at viewer, (breast implants, round breasts, breasts together:1.2), v-cut, shiny breasts, breast focus, ";
-	let backPos = "(view from behind, directly behind, looking away, looking forward:1.5), sideboob, side of head, ";
+	let frontPos = "(front view), leaning back, directly in front, looking at viewer, gigantic breasts, (breast implants, round breasts, breasts together:1.2), shiny breasts, breast focus, ";
+	let backPos = "(view from behind, looking away from viewer, looking straight ahead:1.5), sideboob, (gigantic breasts:1.3), ";
 
 	let baseNeg = "cel shading, flat shading, skindentation, bursting breasts, side view, three-quarters view, closeup, close up, cramped, out of frame, areolas, sweaty, earrings, monochrome, skin, human, human nose, human face, watermark, grayscale, multiple people, more than one, 3 arms, deformed, bad quality, amateur drawing, beginner drawing, bad anatomy, deformed hands, deformed feet, bright hair, missing fingers, extra digit, fewer digits, cropped, very displeasing, bad eyes, deformed eyes, extra marks, extra arms, eye bangs, eye shadow, eye bags, logo, nsfw";
 
+	/*
+	 * create content prompt
+	 */
 	const animal = getRandomOf([ "wolf", "cat", "fox", "bunny", "bear", "otter" ]);
 
 	basePos += `(anthro ${ animal }, ${ animal } ears:1.2), `;
 	basePos += `${colors[0]} fur, ${colors[0]} tail, ${colors[0]} ears, (${colors[0]} skin, ${colors[0]} breasts:1.5), `;
 
 	frontPos += colors[1] + "eyes, ";
-
 	basePos += colors[1] + " hair, ";
 	basePos += getRandomOf([ "long hair", "short hair", "ponytail" ]) + ", ";
 
-	if (getRandom() > 0.8)
-		basePos += "choker, ";
+	basePos += "white button-up shirt, lanyard, black leather pants, ";
+	frontPos += "(single button gap:1.2), ";
 
-	if (getRandom() > 0.8) {
-
-		if (getRandom() > 0.5) {
-			basePos += "white button-up shirt, black pencil skirt, lanyard, ";
-			frontPos += "(single button gap:1.2), ";
-		} else {
-			basePos += getRandomOf(colors) + getRandomOf(["leotard, ", "wedding dress, ", "slingshot bikini, ", "lingerie, lace bra, bra straps, "]);
-		}
-
-	} else {
-		
-		basePos += getRandomOf(colors) + " " + getRandomOf([ "tight t-shirt", "jacket", "sweater", "hoodie", "loose t-shirt", "crop top", "tube top", "bikini top" ]) + ", ";
-		basePos += colors[1] + " " + getRandomOf([ "jean shorts", "yoga pants", "tights", "pleated short skirt", "short pencil skirt", "bikini bottom", "leather pants" ]) + ", ";
-
-		if (getRandom() > 0.5)
-			basePos += getRandomOf([ "witch hat", "chef hat", "crown" ]) + ", ";
-	}
-
-	basePos += getRandomOf([ "hands on hips", "hands behind back" ]) + ", ";
-
-	// output images
+	/*
+	 * generate images
+	 */
 	const image1 = await generateImage({
 		pos: frontPos + basePos,
 		neg: baseNeg,
@@ -121,7 +106,7 @@ async function generatePost(seed) {
 
 	const image2 = await generateImage({
 		pos: backPos + basePos,
-		neg: "front view, looking back at viewer, turning head, " + baseNeg,
+		neg: "(front view, looking back at viewer, looking at viewer, turning head), " + baseNeg,
 		seed: seed + 1,
 		steps: 30,
 		cfg: 6,
