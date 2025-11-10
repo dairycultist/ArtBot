1ff2f29d2759f088a35e1f4535fdbceb36d88c61
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-11-09 22:10:50 -0500

    growth.json

--- auto.js
+++ auto.js
@@ -8,12 +8,10 @@ const fs = require("fs");
 const ask = require("readline-sync");
 const seedrandom = require("seedrandom"); 	// npm install seedrandom
 const { Jimp } = require("jimp"); 			// npm install jimp
-const { intToRGBA } = require("@jimp/utils");
 
 const gradioID = ask.question("Enter gradio ID: ");
 
-// verify gradioID is valid by pinging, if not valid then quit program
-fetch(`https://${ gradioID }.gradio.live/internal/ping`)
+fetch(`https://${ gradioID }.gradio.live/internal/ping`) // verify gradioID is valid by pinging
 .then(async (res) => {
 
     if (res.status != 200)
@@ -29,7 +27,7 @@ fetch(`https://${ gradioID }.gradio.live/internal/ping`)
 
 	count = Math.floor(count);
 
-	const promptJsonString = fs.readFileSync(ask.question("Enter path of prompt JSON (ex. ./prompt.json): "), "utf8");
+	const promptJsonString = fs.readFileSync(ask.question("Enter path to prompt JSON: "), "utf8");
 
 	for (let i = 0; i < count; i++) {
 
@@ -52,45 +50,6 @@ fetch(`https://${ gradioID }.gradio.live/internal/ping`)
 	console.log("\x1b[0m");
     console.error(e + "\x1b[0m");
 });
-
-// const basePromptTree = new Concat(
-
-// 	"<lora:SyMix_NovaFurryXL_illusV10_v01a01:0.5> 1girl",
-// 	"masterpiece, best quality, amazing quality, very aesthetic, absurdres",
-
-// 	"adult, mature, cowboy shot, standing, white background",
-
-// 	new Rand(
-// 		`fluffy fur, anthro ${ animal }, ${ animal } ears, ${colors[0]} fur, ${colors[0]} tail, ${colors[0]} ears`,
-// 		undefined
-// 	),
-
-// 	colors[1] + " hair",
-// 	new Rand("long hair", "short hair", "ponytail"),
-// 	new Rand("straight hair", "wavy hair", "curly hair"),
-
-// 	new Rand("tareme", "tsurime"),
-// 	new Rand(colors[0], colors[1]) + "eyes",
-
-// 	"tight white tshirt, black leggings, midriff exposed"
-
-// 	// new Rand(colors[1] + " v-neck shirt", colors[1] + " sports bra", colors[1] + " hoodie", colors[1] + " sweater"),
-// 	// new Rand("black leather pants", colors[1] + " pencil skirt", colors[1] + " sweatpants")
-// );
-
-// const frontPromptTree = new Concat(
-// 	"front view, wide hips, leaning back, slim, soft smile, looking at viewer, arms at sides"
-// );
-
-// const backPromptTree = new Concat(
-// 	"<lora:HYPv1-4:0.5> front view, wide hips, leaning forward, motion lines, (wide navel), gigantic breasts, thick thighs, venusbody, chubby, bbw, round belly, grabbing belly, looking down, shocked, (belly folds, deep navel, love handles)"
-// );
-
-// /*
-// 	* generate images
-// 	*/
-// const basePos = basePromptTree.evaluate(getRandom);
-// const baseNeg = "(text, male:1.1), leaning on table, lowres, worst quality, bad quality, bad anatomy, jpeg artifacts, signature, watermark";
 
 async function generatePost(prompt, seed) {
 
